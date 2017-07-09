75f8aae123ca2ce8d6a3a1b75d2c3fb27d4fb2ee
7r1n17y <7r1n17y7@gmail.com>
2017-07-08 20:37:50 -0500

    Update Cookie.php
    Add the get function to retrieve the cookie.

--- src/Manage/Cookie.php
+++ src/Manage/Cookie.php
@@ -6,6 +6,14 @@ class Cookie implements Handler {
   public function __construct(array $options = array()) {
     $this->params = session_get_cookie_params();
   }
-  
+  public function get(string $name = null, $default_return_val = null) {
+    if (is_null($name)) {
+      throw new \InvalidArgumentException(sprintf('$name only accepts strings. Input was: "%s"', gettype($name));
+    }
+    if (isset($_COOKIE[$name])) {
+      return $this->decode($COOKIE[$name]);
+    }
+    return $default_return_val;
+  }
 }
 ?>
