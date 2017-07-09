4aa74ab3d11accc3a1f539b9caeb74ffa68bed56
7r1n17y <7r1n17y7@gmail.com>
2017-07-08 20:55:31 -0500

    Update Cookie.php
    Add the send function to the cookie class.

--- src/Manage/Cookie.php
+++ src/Manage/Cookie.php
@@ -7,13 +7,34 @@ class Cookie implements Handler {
     $this->params = session_get_cookie_params();
   }
   public function get(string $name = null, $default_return_val = null) {
-    if (is_null($name)) {
+    if (!is_string($name) || is_null($name)) {
       throw new \InvalidArgumentException(sprintf('$name only accepts strings. Input was: "%s"', gettype($name));
     }
+    if ($name === '') {
+      throw new \InvalidArgumentException('$name can not be empty.');
+    }
     if (isset($_COOKIE[$name])) {
-      return $this->decode($COOKIE[$name]);
+      return $this->_decode($COOKIE[$name]);
     }
     return $default_return_val;
   }
+  public function send(string $name = null, array $options = array('val' => null, 'expire' => 0)) {
+    if (!is_string($name) || is_null($name)) {
+      throw new \InvalidArgumentException(sprintf('$name only accepts strings. Input was: "%s"', gettype($name));
+    }
+    if ($name === '') {
+      throw new \InvalidArgumentException('$name can not be empty.');
+    }
+    $val = isset($options['val']) ? $options['val'] : null;
+    $expire = isset($options['expire']) ? $options['expire'] : 0;
+    if (!is_int($expire) && !is_float($expire)) {
+      throw new \InvalidArgumentException(sprintf('$expire only accepts integers. Input was: "%s"', gettype($expire));
+    }
+		$expire = intval($expire);
+    if ($expire === 0) {
+      $expire = time() - (time() * 2);
+    }
+    setcookie($name, $this->_encode(array('val' => $val)), time() + ($expire), $this->params['path'], $this->params['domain'], $this->params['secure'], $this->params['httponly']);
+	}
 }
 ?>
