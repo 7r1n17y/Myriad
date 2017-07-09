4b41926dc756bdc7a94ddef18cc57dc7e3a634eb
7r1n17y <7r1n17y7@gmail.com>
2017-07-08 20:27:03 -0500

    Update Cookie.php
    Add the class constructor.

--- src/Manage/Cookie.php
+++ src/Manage/Cookie.php
@@ -1,1 +1,11 @@
-
+<?php
+namespace r7r1n17y\Myriad\Manage;
+use r7r1n17y\Myriad\Interfaces;
+class Cookie implements Handler {
+  private $params = array();
+  public function __construct(array $options = array()) {
+    $this->params = session_get_cookie_params();
+  }
+  
+}
+?>
