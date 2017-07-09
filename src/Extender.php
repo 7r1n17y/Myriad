5da89e3c96d614b8d8125a83c84f22f617a144a9
7r1n17y <7r1n17y7@gmail.com>
2017-07-08 22:35:08 -0500

    Create the abstract class.

--- src/Extender.php
+++ src/Extender.php
@@ -1,1 +1,29 @@
-
+<?php
+abstract class Extender {
+  private $_exts = array();
+  public $_this;
+  function __construct() {
+    $_this = $this;
+  }
+  public function add_ext($object) {
+    $this->_exts[] = $object;
+  } 
+  public function __get($var_name) {
+    foreach($this->_exts as $ext) {
+      if (property_exists($ext, $var_name)) {
+        return $ext->$varname;
+      }
+    }
+  }
+  public function __call($method, $args) {
+    foreach($this->_exts as $ext) {
+      if (method_exists($ext, $method)) {
+            return call_user_method_array($method,$ext,$args);
+      }
+        }
+        throw new Exception("This Method {$method} doesn't exists");
+    }
+    
+    
+}
+?>
