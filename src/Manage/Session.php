60d09141170392d55cde761c53e258c8c97dba96
7r1n17y <7r1n17y7@gmail.com>
2017-07-09 22:31:13 -0500

    Update Session.php

--- src/Manage/Session.php
+++ src/Manage/Session.php
@@ -5,44 +5,38 @@
 namespace r7r1n17y\Myriad\Manage;
 use \r7r1n17y\Myriad\Interfaces;
 class Session implements Handler {
-public function get(string $name = null, $default_return_val = 12) {
-		if (!is_string($name) || equals($name, '')) {
-			return null;
-		}
-		if (isset($_SESSION[$name])) {
-			return $_SESSION[$name];
-		}
-		return $default_return_val;
-	}
-	public function send(string $name = null, $val = null, array $options = array()) {
-		if (!is_string($name) || equals($name, '')) {
-			return null;
-		}
-		$_SESSION[$name] = $val;
-	}
-	public function delete(string $name = null) {
-		if (!is_string($name) || equals($name, '')) {
-			return null;
-		}
-		if (isset($_SESSION[$name])) {
-			unset($_SESSION[$name]);
-		}
-	}
-	private function ___destroy() {
-		$this->___reset();
-		$this->cookie->delete(session_name());
-		$this->cookie->delete('token_id');
-		session_destroy();
-	}
-	private function ___started() {
-		if (!equals(php_sapi_name(), 'cli')) {
-			if (version_compare(phpversion(), '5.4.0', '>=')) {
-				return session_status() === PHP_SESSION_ACTIVE ? true : false;
-			} else {
-				return equals(session_id(), '') ? false : true;
-			}
-		}
-		return false;
-	}
+  public function get(string $name = null, $default_return_val = null) {
+    if (!is_string($name) || is_null($name)) {
+      throw new \InvalidArgumentException(sprintf('$name only accepts strings. Input was: "%s"', gettype($name));
+    }
+    if (empty($name)) {
+      throw new \InvalidArgumentException('$name can not be empty.');
+    }
+    if (isset($_SESSION[$name])) {
+      return $_SESSION[$name];
+    }
+    return $default_return_val;
+  }
+  public function send(string $name = null, array $options = array('val' => null)) {
+    if (!is_string($name) || is_null($name)) {
+      throw new \InvalidArgumentException(sprintf('$name only accepts strings. Input was: "%s"', gettype($name));
+    }
+    if (empty($name)) {
+      throw new \InvalidArgumentException('$name can not be empty.');
+    }
+    $val = isset($options['val']) ? $options['val'] : null;
+    $_SESSION[$name] = $val;
+  }
+  public function delete(string $name = null) {
+    if (!is_string($name) || is_null($name)) {
+      throw new \InvalidArgumentException(sprintf('$name only accepts strings. Input was: "%s"', gettype($name));
+    }
+    if (empty($name)) {
+      throw new \InvalidArgumentException('$name can not be empty.');
+    }
+    if (isset($_SESSION[$name])) {
+      unset($_SESSION[$name]);
+    }
+  }
 }
 ?>
