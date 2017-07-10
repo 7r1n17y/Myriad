3faf384995c7d480bfead0ba6a233d5222ac3c4e
7r1n17y <7r1n17y7@gmail.com>
2017-07-09 22:14:04 -0500

    Added comments.

--- src/Manage/Cookie.php
+++ src/Manage/Cookie.php
@@ -1,12 +1,28 @@
 <?php
+/**
+ * @copyright 2017-2017 Aveu Mizura
+ */
 namespace r7r1n17y\Myriad\Manage;
 use r7r1n17y\Myriad\Interfaces, r7r1n17y\Myriad\Helper\Extender as Extender;
 class Cookie extends Extender implements Handler {
+  /** @var mixed[] $params An array containing the cookie params */
   private $params = array();
+  /**
+   * Sets the params varaiable and adds an external helper class
+   * @param mixed[] $options Contains any avaliable configuration options
+   * @return void
+   */
   public function __construct(array $options = array()) {
     $this->params = session_get_cookie_params();
     parent::add_ext(new HelperFunc($options));
   }
+  /**
+   * Gets the cookie requested
+   * @param string $name The name of the cookie we want to get
+   * @param mixed $default_return_val The default return value upon failure
+   * @throws InvalidArgumentException If the name variable is not a string or is empty
+   * @return mixed The value of the cookie requested or the default return value
+   */
   public function get(string $name = null, $default_return_val = null) {
     if (!is_string($name) || is_null($name)) {
       throw new \InvalidArgumentException(sprintf('$name only accepts strings. Input was: "%s"', gettype($name));
@@ -19,6 +35,14 @@ class Cookie extends Extender implements Handler {
     }
     return $default_return_val;
   }
+  /**
+   * Send a cookie
+   * @param string $name The name of the cookie we want to send
+   * @param mixed[] $options An array containing the value we will send and the expire date of the cookie
+   * @throws InvalidArgumentException If the name variable is not a string or is empty
+   * @throws InvalidArgumentException If the expire time is not an integer or can not be converted
+   * @return void
+   */
   public function send(string $name = null, array $options = array('val' => null, 'expire' => 0)) {
     if (!is_string($name) || is_null($name)) {
       throw new \InvalidArgumentException(sprintf('$name only accepts strings. Input was: "%s"', gettype($name));
@@ -37,6 +61,12 @@ class Cookie extends Extender implements Handler {
     }
     setcookie($name, $this->_encode(array('val' => $val)), time() + ($expire), $this->params['path'], $this->params['domain'], $this->params['secure'], $this->params['httponly']);
   }
+  /**
+   * Delete a cookie
+   * @param string $name The name of the cookie we want to delete
+   * @throws InvalidArgumentException If the name variable is not a string or is empty
+   * @return void
+   */
   public function delete(string $name = null) {
     if (!is_string($name) || is_null($name)) {
       throw new \InvalidArgumentException(sprintf('$name only accepts strings. Input was: "%s"', gettype($name));
@@ -49,6 +79,11 @@ class Cookie extends Extender implements Handler {
       setcookie($name, '', time() - 42000, $this->params['path'], $this->params['domain'], $this->params['secure'], $this->params['httponly']);
     }
   }
+  /**
+   * Encode a cookie value
+   * @param mixed[] $options An array that contains the value we want to encode
+   * @return string The cookie value encoded
+   */
   private function _encode(array $options = array('val' => null)) {
     $val = isset($options['val']) ? $options['val'] : null;
     if (is_array($val)) {
@@ -70,6 +105,12 @@ class Cookie extends Extender implements Handler {
     }
     return $res;
   }
+  /**
+   * Decode a cookie string
+   * @param string $encoded The value string we want to decode
+   * @throws InvalidArgumentException If the encoded variable is not a string or is empty
+   * @return mixed The string decoded to its actual value
+   */
   private function _decode(string $encoded = null) {
     if (!is_string($encoded) || is_null($encoded)) {
       throw new \InvalidArgumentException(sprintf('$encoded only accepts strings. Input was: "%s"', gettype($encoded));
