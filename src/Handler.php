2e70c9d2101ae783cdae69d79494279c33c8c348
7r1n17y <7r1n17y7@gmail.com>
2017-07-09 22:40:58 -0500

    Update Handler.php

--- src/Handler.php
+++ src/Handler.php
@@ -1,1 +1,9 @@
-
+<?php
+namespace r7r1n17y\Myriad\Interfaces;
+interface Handler {
+  public function __construct(array $options = array());
+  public function get(string $name = null, $default_return_val = null);
+  public function send(string $name = null, array $options = array('val' => null, 'expire' => 0));
+  public function delete(string $name = null);
+}
+?>
