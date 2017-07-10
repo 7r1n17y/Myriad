325e98ad6ed67a4e648214332d32144e8b28eedf
7r1n17y <7r1n17y7@gmail.com>
2017-07-09 22:38:17 -0500

    Cleaned comments.

--- src/Manage/Session.php
+++ src/Manage/Session.php
@@ -5,6 +5,12 @@
 namespace r7r1n17y\Myriad\Manage;
 use \r7r1n17y\Myriad\Interfaces;
 class Session implements Handler {
+  /**
+   * Get a session variable
+   * @param string $name The name of the session variable we want to get
+   * @throws InvalidArgumentException If the name variable is not a string or is empty
+   * @return mixed The value of the session variable or the defualt return value
+   */
   public function get(string $name = null, $default_return_val = null) {
     if (!is_string($name) || is_null($name)) {
       throw new \InvalidArgumentException(sprintf('$name only accepts strings. Input was: "%s"', gettype($name));
@@ -17,6 +23,13 @@ class Session implements Handler {
     }
     return $default_return_val;
   }
+  /**
+   * Send a session variable
+   * @param string $name The name of the session variable we want to send
+   * @param mixed[] $options An array that contains the value we want to send
+   * @throws InvalidArgumentException If the name variable is not a string or is empty
+   * @return void
+   */
   public function send(string $name = null, array $options = array('val' => null)) {
     if (!is_string($name) || is_null($name)) {
       throw new \InvalidArgumentException(sprintf('$name only accepts strings. Input was: "%s"', gettype($name));
@@ -27,6 +40,12 @@ class Session implements Handler {
     $val = isset($options['val']) ? $options['val'] : null;
     $_SESSION[$name] = $val;
   }
+  /**
+   * Delete a session variable
+   * @param string $name The name of the session variable we want to delete
+   * @throws InvalidArgumentException If the name variable is not a string or is empty
+   * @return void
+   */
   public function delete(string $name = null) {
     if (!is_string($name) || is_null($name)) {
       throw new \InvalidArgumentException(sprintf('$name only accepts strings. Input was: "%s"', gettype($name));
