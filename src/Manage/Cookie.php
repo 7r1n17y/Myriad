5c8dbb264f4763549a3e4c5a642ecfab47e56c81
7r1n17y <7r1n17y7@gmail.com>
2017-07-09 22:17:07 -0500

    Used another namespace.

--- src/Manage/Cookie.php
+++ src/Manage/Cookie.php
@@ -3,7 +3,7 @@
  * @copyright 2017-2017 Aveu Mizura
  */
 namespace r7r1n17y\Myriad\Manage;
-use r7r1n17y\Myriad\Interfaces, r7r1n17y\Myriad\Helper\Extender as Extender;
+use \r7r1n17y\Myriad\Interfaces, \r7r1n17y\Myriad\Helper\Func as HelperFunc, \r7r1n17y\Myriad\Helper\Extender as Extender;
 class Cookie extends Extender implements Handler {
   /** @var mixed[] $params An array containing the cookie params */
   private $params = array();
