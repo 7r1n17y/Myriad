21321f0ce73a7f0662f56a398a7b622c54193f2b
7r1n17y <7r1n17y7@gmail.com>
2017-07-09 22:18:22 -0500

    Update Session.php

--- src/Manage/Session.php
+++ src/Manage/Session.php
@@ -1,1 +1,10 @@
+<?php
+/**
+ * @copyright 2017-2017 Aveu Mizura
+ */
+namespace r7r1n17y\Myriad\Manage;
+use \r7r1n17y\Myriad\Interfaces, \r7r1n17y\Myriad\Helper\Func as HelperFunc, \r7r1n17y\Myriad\Helper\Extender as Extender;
+class Session extends Extender implements Handler {
 
+}
+?>
